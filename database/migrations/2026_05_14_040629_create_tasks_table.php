01b3dd429852ccb7bf9dc2e81c2e7a1e3b7465db
Eduardo Cantero <spliner_6@hotmail.com>
2026-05-14 00:04:02 -0600

    Run migrations on Railway

--- database/migrations/2026_05_14_040629_create_tasks_table.php
+++ database/migrations/2026_05_14_040629_create_tasks_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('tasks', function (Blueprint $table) {
-    $table->id();
-    $table->string('title');
-    $table->text('description');
-    $table->string('status');
-    $table->timestamps();
-});
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status');
+            $table->timestamps();
+        });
     }
 
     /**
